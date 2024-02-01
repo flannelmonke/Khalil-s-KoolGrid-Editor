0d57efbb5c4c04b3b8f35e129ecd7b8f91a7fcac
flannelmonke <khalilanaji@gmail.com>
2024-01-31 23:06:30 -0500

    Add file open functionality and update page title

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,50 @@
-const { app, BrowserWindow } = require("electron");
+// import { load_file } from "./file_scripts/open_file.js";
+const { app, BrowserWindow, dialog, Menu } = require("electron");
+
+function load_file(Path, BrowserWindow, dialog) {
+  let file = file.target.files[0];
+  if (file) {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      let contents = e.target.result;
+      BrowserWindow.webContents.send("file-opened", Path, contents);
+    };
+    console.log("File read successfully" + file + " " + reader.result);
+  } else {
+    console.log("Error: File not found");
+  }
+}
+
+const menuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Open File",
+        click: () => {
+          dialog
+            .showOpenDialog(BrowserWindow.getFocusedWindow(), {
+              properties: ["openFile"],
+            })
+            .then((result) => {
+              if (!result.canceled) {
+                let file = result.filePaths[0];
+                load_file(file, BrowserWindow, dialog);
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        },
+      },
+      // other File menu items go here
+    ],
+  },
+  // other top-level menu items go here
+];
+
+const menu = Menu.buildFromTemplate(menuTemplate);
+Menu.setApplicationMenu(menu);
 
 const createWindow = () => {
   const win = new BrowserWindow({
